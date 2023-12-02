de12d21ba555730fe60b69c8ced4ee022a61f46e
c4chula <henry.foster71@yahoo.com>
2023-12-03 00:19:53 +0300

    created sqlalchemy engine for sqlite with aioslite and added user repo and service

---
+++ src/tg_assistant/users/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String
+from sqlalchemy.dialects.sqlite import TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.tg_assistant.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_telegram_id: Mapped[str] = mapped_column(String(512))
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.now)
