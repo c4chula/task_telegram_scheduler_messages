fcfb06d5679bbe1c0cedc0db38b2e58b27f46789
c4chula <henry.foster71@yahoo.com>
2023-12-03 10:46:23 +0300

    put logger into __init__.py

--- src/tg_assistant/app.py
+++ src/tg_assistant/app.py
@@ -1,10 +1,8 @@
 import uvloop
 
-from tg_assistant.client import client
-from tg_assistant.logger import logger
-
 uvloop.install()
 
+from tg_assistant.client import client  # noqa: E402
+
 if __name__ == "__main__":
-    logger.info("telegram client start working!)")
     client.run()
