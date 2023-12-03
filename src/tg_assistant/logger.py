b72eb9893a428cd576a6375f5052dceb4ebd742c
c4chula <henry.foster71@yahoo.com>
2023-12-03 10:41:38 +0300

    removed second logger handler

--- src/tg_assistant/logger.py
+++ src/tg_assistant/logger.py
@@ -1,8 +1,5 @@
-import sys
-
 from loguru import logger
 
-logger.add(sys.stdout, format="{time} | {level: <8} | {message}", level="DEBUG")
 logger.add(
     "logs.log",
     format="{time} | {level: <8} | {name: ^15} | {function: ^15} | {line: >3} | {message}",
